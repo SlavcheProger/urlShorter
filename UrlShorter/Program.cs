5caff8b05f7fc883c91b034e722bd1f57aeef47e
slavche <v.feofanov@medved.studio>
2018-06-30 08:28:38 +0300

    Little changes

--- UrlShorter/Program.cs
+++ UrlShorter/Program.cs
@@ -29,23 +29,23 @@ namespace UrlShorter
 
         public static string CreateShortUrl(string username, string apiKey, string longUrl)
         {
-            XmlDocument xmlDoc = new XmlDocument();
+            var xmlDoc = new XmlDocument();
 
-            WebRequest request = WebRequest.Create("http://api.bitly.com/v3/shorten");
-            byte[] data = Encoding.UTF8.GetBytes(string.Format("login={0}&apiKey={1}&longUrl={2}&format={3}",
+            var request = WebRequest.Create("http://api.bitly.com/v3/shorten");
+            var data = Encoding.UTF8.GetBytes(string.Format("login={0}&apiKey={1}&longUrl={2}&format={3}",
                 username, apiKey, HttpUtility.UrlEncode(longUrl), "xml"));
 
             request.Method = "POST";
             request.ContentType = "application/x-www-form-urlencoded";
             request.ContentLength = data.Length;
 
-            using (Stream stream = request.GetRequestStream())
+            using (var stream = request.GetRequestStream())
             {
                 stream.Write(data, 0, data.Length);
             }
-            using (WebResponse response = request.GetResponse())
+            using (var response = request.GetResponse())
             {
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                using (var reader = new StreamReader(response.GetResponseStream()))
                 {
                     xmlDoc.LoadXml(reader.ReadToEnd());
                 }
