d1c1729992ab331b23b5e59218ad2b1047c79ba8
slavche <v.feofanov@medved.studio>
2018-06-30 08:23:08 +0300

    Added async-await construction

--- UrlShorter/Program.cs
+++ UrlShorter/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Xml;
 
@@ -11,21 +12,23 @@ namespace UrlShorter
     {
         static void Main(string[] args)
         {
-            string username = "slavche001";
-            string apiKey = "R_82581e4222df41e3859b3262f1d2d3f0";
-            string longUrl = "https://www.youtube.com/results?search_query=bitly+api+key+c%23";
-            string shortUrl = CreateShortUrl(username, apiKey, longUrl);
+            var username = "slavche001";
+            var apiKey = "R_82581e4222df41e3859b3262f1d2d3f0";
+            var longUrl = "https://www.youtube.com/results?search_query=async+await+c%23";
+
+            var shortUrl = ShortUrlGenerator(username, apiKey, longUrl).Result;
 
             Console.WriteLine(shortUrl);
             Console.ReadKey();
         }
 
+        public static async Task<string> ShortUrlGenerator(string username, string apiKey, string longUrl)
+        {
+            return await Task.FromResult<string>(CreateShortUrl(username, apiKey, longUrl));
+        }
+
         public static string CreateShortUrl(string username, string apiKey, string longUrl)
         {
-            string statusCode = string.Empty;
-            string statusText = string.Empty;
-            string shortUrl = string.Empty;
-
             XmlDocument xmlDoc = new XmlDocument();
 
             WebRequest request = WebRequest.Create("http://api.bitly.com/v3/shorten");
@@ -36,21 +39,19 @@ namespace UrlShorter
             request.ContentType = "application/x-www-form-urlencoded";
             request.ContentLength = data.Length;
 
-            using (Stream ds = request.GetRequestStream())
+            using (Stream stream = request.GetRequestStream())
             {
-                ds.Write(data, 0, data.Length);
+                stream.Write(data, 0, data.Length);
             }
             using (WebResponse response = request.GetResponse())
             {
-                using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
                 {
-                    xmlDoc.LoadXml(sr.ReadToEnd());
+                    xmlDoc.LoadXml(reader.ReadToEnd());
                 }
             }
 
-            shortUrl = xmlDoc.GetElementsByTagName("url")[0].InnerText;
-
-            return shortUrl;
+            return xmlDoc.GetElementsByTagName("url")[0].InnerText;
         }
     }
 }
